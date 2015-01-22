a7be2edfb6ee199d5e85b9a355fe99add0eccf42
Matt Constantine <matt@zaarly.com>
2015-01-21 20:37:38 -0800

    Corrected controller.

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ var SocketIo = require('socket.io-client'),
 
 socket.on('connect', function() {
   socket.on('note', function(m) {
-    if (m.status === COMMAND && m.note === 1) {
+    if (m.status === COMMAND && m.note === 16) {
+    console.log("m", m)
       lx.bulbs[0] && lx.bulbs[0].set(HUE, SAT, m.velocity / 127, 0, 2);
       lx.bulbs[1] && lx.bulbs[1].set(HUE, SAT, m.velocity / 127, 0, 2);
     }
